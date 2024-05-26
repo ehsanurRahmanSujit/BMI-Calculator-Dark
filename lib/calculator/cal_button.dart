93a56b399bb46f8ca8a4854bcb9f6fed0f968ca6
Ehsanur Rahman <sujit.ehsanur@gmail.com>
2024-05-26 13:40:13 +0600

    upload project

---
+++ lib/calculator/cal_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../palette.dart';
+
+class CalButton extends StatelessWidget {
+  const CalButton({
+    super.key,
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 12,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: Palette.action,
+      ),
+      child: TextButton(
+        style: ButtonStyle(
+          overlayColor: WidgetStateProperty.all(Colors.white.withOpacity(0.10)),
+        ),
+        onPressed: onTap,
+        child: const Text(
+          'CALCULATE YOUR BMI',
+          style: TextStyle(
+            color: Palette.textActive,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bmi_calculator/calculator/cal_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Prevent landscape orientation
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      home: CalPage(title: 'BMI CALCULATOR'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
